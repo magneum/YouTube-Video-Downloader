c1828b4b28289ac55faa7eff0a47de873eaaf658
HypeVoid Soul <hypevoidsoul@gmail.com>
2021-07-07 14:21:52 +0530

    await ɦʋֆ.delete()

--- ʏօʊɛʟʍӼ/YTLIBP/YTLIBP.py
+++ ʏօʊɛʟʍӼ/YTLIBP/YTLIBP.py
@@ -23,10 +23,9 @@ async def ytdl(_,ɦʋֆ: Message):
             return
     except:
         pass
-    
     url = ɦʋֆ.text.strip()
-    await ɦʋֆ.reply_chat_action(CRAV)
-    await ɦʋֆ.delete()
+    #await ɦʋֆ.reply_chat_action(CRAV)
+    #await ɦʋֆ.delete()
     await ɦʋֆ.reply_chat_action(CRAV)
     title, fetchedimage, formats = ytget_lib(url)
     user_time[ɦʋֆ.chat.id] = datetime.now() + \
@@ -54,3 +53,4 @@ async def ytdl(_,ɦʋֆ: Message):
         except Exception as e:
             await ɦʋֆ.reply_text(
             f"<code>{e}</code> #Error")
+            await ɦʋֆ.delete()
