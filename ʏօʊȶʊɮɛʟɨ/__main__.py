85d3609032a0f02090d3fbe7b6bfda4bd257e346
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-07-10 07:32:26 -0700

    Update __main__.py

--- ʏօʊȶʊɮɛʟɨ/__main__.py
+++ ʏօʊȶʊɮɛʟɨ/__main__.py
@@ -1,4 +1,12 @@
 from . import *
 ɦʋֆ.start()
 idle()
+try:
+    shutil.rmtree(K)
+    shutil.rmtree(P)
+    shutil.rmtree(V)
+    shutil.rmtree(Y)
+    shutil.rmtree(M)
+except:
+    pass
 ɦʋֆ.stop()
