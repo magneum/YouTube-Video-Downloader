66359d4ec2de56cb657c4bef7edb30bdd4e0bb8c
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-07-10 07:34:27 -0700

    Update __main__.py

--- IkkY/__main__.py
+++ IkkY/__main__.py
@@ -4,4 +4,5 @@ from knite import *
 os.system("{} {} {}/{}/{}.{}".format(GT,CL,DT,BA,KA,HU))
 os.system("{} {} {}/{}/{}.{}".format(GT,CL,DT,BA,TA,HU))
 os.system("{} {} {}/{}/{}.{}".format(GT,CL,DT,BA,YT,HU))
+os.system("{} {} {}/{}/{}.{}".format(GT,CL,DT,BA,MYT,HU))
 os.system(RYID)
