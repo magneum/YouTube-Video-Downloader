2270089e0cfa301c99ec04799acfc94999190fd6
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-07-10 23:19:31 -0700

    from pyrogram.methods.utilities import idle

--- ʏօʊȶʊɮɛʟɨ/__init__.py
+++ ʏօʊȶʊɮɛʟɨ/__init__.py
@@ -1,18 +1,2 @@
-from Cula import *
-from ʟaʄɨօ import *
 
-plugins = dict(
-    root="ʏօʊɛʟʍӼ",
-)
-TOKEN = Kryogenesis.TOKEN
-APP_ID = Kryogenesis.APP_ID
-API_HASH = Kryogenesis.API_HASH
 
-Client = Client(
-    session_name="ɦʋֆ",
-    bot_token=TOKEN,
-    api_id=APP_ID,
-    api_hash=API_HASH,
-    plugins=plugins,
-    workers=12
-    )
--- ʏօʊȶʊɮɛʟɨ/__main__.py
+++ ʏօʊȶʊɮɛʟɨ/__main__.py
@@ -1,4 +1,24 @@
-from . import *
+from pyrogram.methods.utilities import idle
+from Cula import *
+from ʟaʄɨօ import *
+
+plugins = dict(
+    root="ʏօʊɛʟʍӼ",
+)
+TOKEN = Kryogenesis.TOKEN
+APP_ID = Kryogenesis.APP_ID
+API_HASH = Kryogenesis.API_HASH
+
+Client = Client(
+    session_name="ɦʋֆ",
+    bot_token=TOKEN,
+    api_id=APP_ID,
+    api_hash=API_HASH,
+    plugins=plugins,
+    workers=12
+    )
+
+
 Client.start()
 idle()
 try:
