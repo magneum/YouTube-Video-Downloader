a8533c29cb18de0a73577bc4737453a124e37ade
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-09-05 10:24:26 +0530

    +bestaudio[ext=m4a]/best[ext=mp4]
    +bestaudio[ext=m4a]/best[ext=mp4]

--- 𝐘𝐨𝐮𝐓𝐮𝐛𝐞🎬𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐞𝐫/__main__.py
+++ 𝐘𝐨𝐮𝐓𝐮𝐛𝐞🎬𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐞𝐫/__main__.py
@@ -292,8 +292,9 @@ has been licensed under GNU General Public License                              
 "|"
 "|"
 "|"
+#+bestaudio[ext=m4a]/best[ext=mp4]
 VOIDED = YouTube_Opts = {
-'format': "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]",
+'format': "bestvideo[ext=mp4]",
 'outtmpl': '%(title)s - %(extractor)s-%(id)s.%(ext)s',
 "no_warnings": True,
 "ignoreerrors": True,
